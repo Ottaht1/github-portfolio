97874ef26d2a648d4960d5f2f09c14ca5ade1414
Theresa Ottah <ottahtheresa@gmail.com>
2022-11-07 02:40:51 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import App from "./App";
-import "./App.css";
-
-const App = () => (
-  <div>
-    <h1> Repositories</h1>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </div>
+  </React.StrictMode>
 );
 
-render(<App />, document.getElementById("root"));
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
