a6c89e232973c4e2043a602cc579fa3130b212a4
Theresa Ottah <ottahtheresa@gmail.com>
2023-10-16 20:15:05 +0100

    fix:responsivness on smaller screens

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "./Button";
 
 function Profile() {
@@ -20,9 +20,8 @@ function Profile() {
       })
       .catch((error) => {
         console.error("Error fetching GitHub profile data:", error);
-        
       });
-  }, []); 
+  }, []);
 
   if (!profile) {
     return <div>Loading...</div>;
@@ -30,35 +29,55 @@ function Profile() {
 
   return (
     <div className="profile-container">
-    <div className="profile">
-      <img src={profile.avatar_url} alt="Profile" />
-      <h2>{profile.login}</h2>
-      <h2>{profile.name}</h2>
-    <p>{profile.bio}</p>
-    <p>Location: {profile.location}</p>
-    {profile.email && <p>Email: {profile.email}</p>}
+      <div className="profile">
+        <img src={profile.avatar_url} alt="Profile" />
+        <h2>{profile.login}</h2>
+        <h2>{profile.name}</h2>
+        <p>{profile.bio}</p>
+        <p>Location: {profile.location}</p>
+        {profile.email && <p>Email: {profile.email}</p>}
 
-    <p>Followers: {profile.followers}</p>
-    <p>Following: {profile.following}</p>
-    <p>Repositories: {profile.public_repos}</p>
-    <p>Twitter: <a href={`https://twitter.com/${profile.twitter_username}`}>{profile.twitter_username}</a></p>
-    <p>LinkedIn: <a href={`https://www.linkedin.com/in/theresa-ottah/`}>Theresa Ottah</a></p>
-
-    </div>
-    <div className="additional-info">
+        <p>Followers: {profile.followers}</p>
+        <p>Following: {profile.following}</p>
+        <p>Repositories: {profile.public_repos}</p>
+        <p>
+          Twitter:{" "}
+          <a href={`https://twitter.com/${profile.twitter_username}`}>
+            {profile.twitter_username}
+          </a>
+        </p>
+        <p>
+          LinkedIn:{" "}
+          <a href={`https://www.linkedin.com/in/theresa-ottah/`}>
+            Theresa Ottah
+          </a>
+        </p>
+      </div>
+      <div className="additional-info">
         {/* Display additional information here */}
         <h2>Hi 👋, I'm Theresa!</h2>
         <p>A frontend developer from Nigeria</p>
         <p>🌱 I’m currently learning JavaScript and React</p>
         <p>👯 I’m looking to collaborate on interesting projects</p>
-        <p>📫 How to reach me <a href="mailto:ottahtheresa@gmail.com">ottahtheresa@gmail.com</a></p>
+        <div className="email-info">
+          <p>
+            How to reach me{" "}
+            <a href="mailto:ottahtheresa@gmail.com">ottahtheresa@gmail.com</a>
+          </p>
+        </div>
         <p>Connect with me:</p>
-        <ul style={{listStyle: "none"}}>
-          <li><a href="https://twitter.com/__theressaa">__theressaa</a></li>
-          <li><a href="https://www.linkedin.com/in/theresa-ottah/">theresa-ottah</a></li>
+        <ul style={{ listStyle: "none" }}>
+          <li>
+            <a href="https://twitter.com/__theressaa">__theressaa</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/theresa-ottah/">
+              theresa-ottah
+            </a>
+          </li>
         </ul>
         <p>Languages and Tools:</p>
-        <ul style={{listStyle: "none"}}>
+        <ul style={{ listStyle: "none" }}>
           <li>html5</li>
           <li>css3</li>
           <li>javascript</li>
@@ -68,14 +87,15 @@ function Profile() {
           <li>bootstrap</li>
           <li>typescript</li>
         </ul>
-        
       </div>
 
       {/* Buttons to navigate to Repositories and back to Profile */}
       <Link to="/repos/home">
-        <Button text="View Repositories  ➞" className="view-repositories-button" />
+        <Button
+          text="View Repositories  ➞"
+          className="view-repositories-button"
+        />
       </Link>
-
     </div>
   );
 }
